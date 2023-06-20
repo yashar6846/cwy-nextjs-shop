4f43fc5cef073e97941e72ccc159e59c15ea12cf
Yashar Najafi <yasharnajafi6946@gmail.com>
2023-06-20 14:10:22 +0300

    add api and fierbase

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,62 @@
+import { useRouter } from 'next/router';
+import Button from '../../components/ui/button';
+
+import { getFilteredEvents } from '../../dummy-data';
+import EventList from '../../components/events/event-list';
+
 function FilteredEventsPage() {
+
+    const router = useRouter();
+    const filterData = router.query.slug;
+    
+    if(!filterData) {
+        return <p className='center'>Loading...</p>
+    }
+
+    const filteredYear = filterData[0];
+    const filteredMonth = filterData[1];
+
+    const numYear = +filteredYear;
+    const numMonth = +filteredMonth;
+
+    if(
+        isNaN(numYear) ||
+        isNaN(numMonth) ||
+        numYear > 2030 ||
+        numYear < 2021 ||
+        numMonth > 12 ||
+        numMonth < 1
+    ) {
+        return (
+            <>
+                <p>Invalid filter, Please adjust your values!</p>
+                <div className='center'>
+                    <Button link='/events'>Show All evenets</Button>
+                </div>
+            </>
+        )
+    }
+
+    const filteredEevents = getFilteredEvents({
+        year: numYear,
+        month: numMonth,
+    });
+
+    if(!filteredEevents || filteredEevents.length === 0){
+        return (
+            <>
+                <p>No events found for the chosen filter!</p>
+                <div className='center'>
+                    <Button link='/events'>Show All evenets</Button>
+                </div>
+            </>
+        )
+    }
+
+
     return (
         <div>
-            <h1>Filtered Events Page</h1>
+            <EventList items={filteredEevents} />
         </div>
     );
 }
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,19 @@
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
-
 import {getAllEvents} from "@/dummy-data";
+import { useRouter } from "next/router";
 
 function AllEventsPage() {
     const events = getAllEvents();
+    const router = useRouter();
+
+    const findEventsHandler =(year, month)=>{
+      const fullPath = `/events/${year}/${month}`
+      router.push(fullPath)
+    }
     return (
         <>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHandler} />
             <EventList items={events} />
         </>
     );
